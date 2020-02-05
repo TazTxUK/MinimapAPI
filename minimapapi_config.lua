55a94be0dd1c7c8665887f46bc552d3254988d94
Taz <tazstar987@hotmail.com>
2020-02-05 18:18:03 +0000

    Changed default semivisible config option to true

--- minimapapi_config.lua
+++ minimapapi_config.lua
@@ -11,8 +11,8 @@ MinimapAPI.Config = {
 	["PositionY"] = 4, 					--Default = 4
 	["DisplayMode"] = 2,				--Default = 2
 	["ShowLevelFlags"] = true,			--Default = true
-	["SmoothSlidingSpeed"] = 0.5,			--Default = 1		(1 is instantaneous, 0.1 is extremely slow)
+	["SmoothSlidingSpeed"] = 0.5,		--Default = 0.5		(1 is instantaneous, 0.1 is extremely slow)
 	["HideInCombat"] = 1,				--Default = 1
 	["OverrideVoid"] = false,			--Default = false
-	["DisplayExploredRooms"] = false,	--Default = false
+	["DisplayExploredRooms"] = true,	--Default = true
 }
