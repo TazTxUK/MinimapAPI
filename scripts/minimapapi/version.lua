76b3a92a1cd5a4512f8ae52d01aad2bec0ef53b4
Taz <tazstar987@hotmail.com>
2020-02-06 20:19:36 +0000

    Changed version

--- scripts/minimapapi/version.lua
+++ scripts/minimapapi/version.lua
@@ -1,7 +1,1 @@
-MinimapAPI.Version = "MinimapAPI 1.1 - Taz Wofsauge"
---[[
-If you are making your own version of MinimapAPI (for integration or otherwise), please change the version.
-I.E "MinimapAPI 1.1 BobMod - MrBob3268"
-
-MinimapAPI does not run if another version is already running.
-]]
+MinimapAPI.Version = "master"
