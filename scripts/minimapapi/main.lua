7adbade1f6848aabfe0664b8bae4fd0afb0998bb
kittenchilly <mario.pikmin.zelda@gmail.com>
2023-01-10 14:10:19 -0600

    Update main.lua

--- scripts/minimapapi/main.lua
+++ scripts/minimapapi/main.lua
@@ -251,7 +251,7 @@ function MinimapAPI:GetDoorSlotValue(doorgroup, doordir)
 end
 
 local defaultCustomPickupPriority = 14999 --more than vanilla, less than other potential custom pickups
-function MinimapAPI:AddPickup(id, iconid, typ, variant, subtype, call, icongroup, priority, strict)
+function MinimapAPI:AddPickup(id, iconid, typ, variant, subtype, call, icongroup, priority, condition)
 	local newPickup
 	if type(id) == "table" and iconid == nil then
 		local t = id
