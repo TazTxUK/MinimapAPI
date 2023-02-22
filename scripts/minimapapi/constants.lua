bafb7072206bc3d0d0508a8036c000c37b3a6897
James <5511220+Zamiell@users.noreply.github.com>
2023-02-22 16:13:56 -0500

    Update constants.lua

--- scripts/minimapapi/constants.lua
+++ scripts/minimapapi/constants.lua
@@ -1,6 +1,6 @@
 local constants = {}
 
--- It is conventional in the Isaac ecosystem for libraries to take `CallbackPriority.IMPORTANT`.
+-- It is conventional in the Isaac ecosystem for libraries to use `CallbackPriority.IMPORTANT`.
 -- However, MinimapAPI should run before other libraries.
 constants.CALLBACK_PRIORITY = CallbackPriority.IMPORTANT - 1
 
