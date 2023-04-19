df45b36694c66763f1edebac8bae9d75e4cc5c43
Wofsauge <jan-.-@t-online.de>
2023-04-19 19:22:22 +0200

    Fixed AB+ incompatibility

--- scripts/minimapapi/constants.lua
+++ scripts/minimapapi/constants.lua
@@ -2,7 +2,7 @@ local constants = {}
 
 -- It is conventional in the Isaac ecosystem for libraries to use `CallbackPriority.IMPORTANT`.
 -- However, MinimapAPI should run before other libraries.
-constants.CALLBACK_PRIORITY = CallbackPriority.IMPORTANT - 1
+constants.CALLBACK_PRIORITY = REPENTANCE and (CallbackPriority.IMPORTANT - 1) or 0
 constants.STAGEAPI_CALLBACK_PRIORITY = 0.5 --Most mods use Priority of 1, while StageAPI uses 0, this will run in-between those
 
 return constants
