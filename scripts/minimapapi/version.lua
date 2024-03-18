80695ab97f2e8e74fd0cf1be3429e693365fec6d
Wofsauge <jan-.-@t-online.de>
2024-03-18 23:18:28 +0100

    (V2.47) WORKSHOP RELEASE 18.03.2024 - 23:16

--- scripts/minimapapi/version.lua
+++ scripts/minimapapi/version.lua
@@ -2,4 +2,4 @@ local MinimapAPI = require("scripts.minimapapi")
 
 MinimapAPI.BranchVersion = "Master" --branch
 MinimapAPI.MajorVersion = 2
-MinimapAPI.MinorVersion = 46
+MinimapAPI.MinorVersion = 47
